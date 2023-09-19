782057860c19bb0bdc5dac1d9feef0f57deda194
Alin Triff <triffalin@gmail.com>
2023-09-19 17:51:46 +0300

    inserted the logical for checking for a winner or draw

--- scripts/game.js
+++ scripts/game.js
@@ -36,5 +36,54 @@ function selectGameField(event) {
 
 	gameData[selectedRow][selectedColumn] = activePlayer + 1;
 
+	const winnerId = checkForGameOver();
+	console.log(winnerId);
+
+	currentRound++;
 	switchPlayer();
+}
+
+function checkForGameOver() {
+	// Checking the rows for equality
+	for (let i = 0; i < 3; i++) {
+		if (
+			gameData[i][0] > 0 &&
+			gameData[i][0] === gameData[i][1] &&
+			gameData[i][1] === gameData[i][2]
+		) {
+			return gameData[i][0];
+		}
+	}
+	// Checking the columns for equality
+	for (let i = 0; i < 3; i++) {
+		if (
+			gameData[0][i] > 0 &&
+			gameData[0][i] === gameData[1][i] &&
+			gameData[0][i] === gameData[2][i]
+		) {
+			return gameData[0][i];
+		}
+	}
+	// Checking the diagonals, top left to bottom right for equality
+	if (
+		gameData[0][0] > 0 &&
+		gameData[0][0] === gameData[1][1] &&
+		gameData[1][1] === gameData[2][2]
+	) {
+		return gameData[0][0];
+	}
+	// Checking the diagonals, bottom left to top right for equality
+	if (
+		gameData[2][1] > 0 &&
+		gameData[2][0] === gameData[1][1] &&
+		gameData[1][1] === gameData[0][2]
+	) {
+		return gameData[2][0];
+	}
+
+	if (currentRound === 9) {
+		return -1;
+	}
+
+	return 0;
 }
