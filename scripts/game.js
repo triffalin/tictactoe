36c3cfa72424d480a043c7c731cc633ee43cf5c5
Alin Triff <triffalin@gmail.com>
2023-09-19 11:57:16 +0300

    fixed bug on function startNewGame

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,5 @@
 function startNewGame() {
-	if (players[0].name === '' || players[1]) {
+	if (players[0].name === '' || players[1].name === '') {
 		alert('Please set custom player names for both players!');
 		return;
 	}
